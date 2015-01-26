0c7090fbbab34a341696eda756b21b152af080d5
smalllixin <smalllixin@gmail.com>
2015-01-26 19:30:51 +0800

    project create

---
+++ LxTaskQueue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LxTaskQueue
+//
+//  Created by lixin on 1/26/15.
+//  Copyright (c) 2015 lxtap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LxTaskQueue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LxTaskQueue
+//
+//  Created by lixin on 1/26/15.
+//  Copyright (c) 2015 lxtap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
